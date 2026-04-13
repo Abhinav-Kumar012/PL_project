544df89e840cf3b434d0ff6096734d3a4e30d7ca
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-04-13 23:35:04 +0530

    learned missing_method - 2

--- ruby/meta_prog_3.rb
+++ ruby/meta_prog_3.rb
@@ -2,20 +2,25 @@ class Square
   def initialize()
     
   end
-    def method_missing(method_name, *args, &block)
-        if method_name.to_s.start_with?("area_")
-            length = method_name.to_s.gsub("area_", "").to_i
-            return length*length
-        else
-            super
-        end
+  def method_missing(method_name, *args, &block)
+    method_n = method_name.to_s
+    if method_n.start_with?("area_")
+      length = method_n.gsub("area_", "").to_i
+      return length*length
+    elsif method_n.start_with?("perimeter_")
+      length = method_n.gsub("perimeter_", "").to_i
+      return 4*length
+    else
+        super
     end
+  end
 end
 
 
 def main
   p = Square.new()
   puts p.area_50000
+  puts p.perimeter_50000
 end
 
 if __FILE__ == $0
