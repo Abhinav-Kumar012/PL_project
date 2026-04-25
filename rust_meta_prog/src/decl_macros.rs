d4e98347d4d9d4c84a0f6b92ed5c65bd85e70c0b
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-04-25 15:29:43 +0530

    added getter and setter example in rust - 1

--- rust_meta_prog/src/decl_macros.rs
+++ rust_meta_prog/src/decl_macros.rs
@@ -18,5 +18,40 @@ mod decl_macros {
         };
     }
 
+    #[macro_export]
+    macro_rules! getter_and_setter {
+        ($struct_vis : vis struct $struct_id : ident {$($field_vis : vis $field : ident : $type : ty),* $(,)?}) => {
+            $struct_vis struct $struct_id {
+                $(
+                    $field_vis $field : $type
+                ),*
+            }
+            impl $struct_id {
+                pub fn new($($field : $type),*) -> Self{
+                    return Self {
+                        $(
+                            $field
+                        ),*
+                    }
+                }
+                pastey::paste!{
+                    $(
+                        pub fn [<get_ $field>](&self) -> &$type {
+                            return &(self.$field)
+                        }
+                        pub fn [<get_mut_ $field>](&mut self) -> &mut $type {
+                            return &mut (self.$field)
+                        }
+                        pub fn [<set_ $field>](&mut self, new_val : $type) {
+                            self.$field = new_val;
+                        }
+                    )*
+                }
+            }
+        };
+    }
+
 	pub(crate) use area_and_perimeter;
+    pub(crate) use getter_and_setter;
+
 }
--- rust_meta_prog/src/main.rs
+++ rust_meta_prog/src/main.rs
@@ -1,8 +1,16 @@
 mod decl_macros;
 struct Square;
 area_and_perimeter!(Square, 10, 21, 30);
+getter_and_setter!(pub struct Point {
+    pub x : i32,
+    pub y : i32,
+    pub z : i32,
+    pub label : String,
+});
 
 fn main() {
+    let p = Point::new(23, 32,78,"vector 1".to_string());
+    println!("{}", p.get_x());
 	println!("{}", Square::area_10());
 	println!("{}", Square::perimeter_21());
 }
