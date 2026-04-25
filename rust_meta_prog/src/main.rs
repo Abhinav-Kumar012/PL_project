b462882341152e2707207680b811a0cb73397a72
Abhinav Kumar <Abhinav.Kumar@iiitb.ac.in>
2026-04-25 12:24:49 +0530

    rust meta prog - 1

--- rust_meta_prog/src/main.rs
+++ rust_meta_prog/src/main.rs
@@ -1,3 +1,28 @@
+use pastey::paste;
+
+macro_rules! multi_fxn_def {
+    ($struct_id:ty, $($x:expr),* $(,)?) => {
+        impl $struct_id {
+            paste! {
+                $(
+                    pub fn [<area_ $x>]() -> isize {
+                        $x * $x
+                    }
+
+                    pub fn [<perimeter_ $x>]() -> isize {
+                        4 * $x
+                    }
+                )*
+            }
+        }
+    };
+}
+
+struct Square;
+
+multi_fxn_def!(Square, 10,21, 30);
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", Square::area_10());
+    println!("{}", Square::perimeter_21());
 }
